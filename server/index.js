1f4d40dbf2c994690990125bd99b757d801cbd87
BangKartavya <bangkartavya@gmail.com>
2024-11-01 16:44:08 +0530

    server changes

--- server/index.js
+++ server/index.js
@@ -12,8 +12,14 @@ app.use(bodyParser.json({limit : "30mb", extended : true}));
 app.use(bodyParser.urlencoded({limit : "30mb", extended : true}));
 app.use(cors());
 
+
+
 app.use("/posts", postRoutes);
 app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Memories API");
+});
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
