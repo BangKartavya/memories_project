d97155b7eeba8ea4709bafb1a3c685d2e864914d
BangKartavya <bangkartavya@gmail.com>
2024-11-01 17:04:24 +0530

    quality of life increased

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -36,7 +36,7 @@ const Post = ({ post, setCurrentId }) => {
 
     const Likes = () => {
         if (likes.length > 0) {
-            return hasLikedPost ? (
+            return likes.find((like) => like === userId) ? (
                 <><ThumbUpAltIcon fontSize="small" />&nbsp; {likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}`} </>
             ) : (
                 <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
